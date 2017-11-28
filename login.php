11ac915d62ce330490c1dc8fc68be772d3463484
Joe Caple <32098792+Caple-JC-BPC-Magenta@users.noreply.github.com>
2017-11-28 14:26:02 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+
+$uname=$_POST['username'];
+$pass=$_POST ['password'];
+
+if($uname=="awesome"&&$pass=="123"
+   echo "content after login has been sucessful.";
+   
+   else
+   
+   echo"invalid";
+   
+   ?>
